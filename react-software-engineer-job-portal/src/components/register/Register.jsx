6f676c8e3031605d9151420b79ccc0824bd58f6a
Elliott Tay <austonmartin@gmail.com>
2022-09-08 21:58:34 +0800

    fixed registration and login

--- react-software-engineer-job-portal/src/components/register/Register.jsx
+++ react-software-engineer-job-portal/src/components/register/Register.jsx
@@ -1,33 +1,38 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Slider from '@mui/material/Slider';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-
-
-
+import * as React from "react";
+import {useState} from 'react';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Software Engineered
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -35,13 +40,87 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Register() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    job: "",
+    position: "",
+    experience: 0,
+    // skills: 'CSS, HTML, React'
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: formData.email,
+      password: formData.password,
+      name: formData.name,
+      job: formData.job,
+      position: formData.position,
+      // skills: formData.skills
     });
+    // fetch(`http://localhost:3000/register`, {
+    //   method: "POST",
+    //   body: JSON.stringify(formData),
+    //   headers: {
+    //     "Content-type": "application/json",
+    //   },
+    // })
+    //   .then(
+    //     // (response) => {
+    //     navigate("/login")
+        // console.log(response);
+        // return response.json();
+      // }
+      // )
+      // .then((jsonResponse) => {
+      //   if (jsonResponse.error) {
+      //     console.log(jsonResponse.error);
+      //     toast.error(jsonResponse.error);
+      //     return;
+      //   }
+        // navigate("/login");
+      // });
+
+
+      // Need to add the mongodb here?
+      fetch(`http://localhost:3000/register`, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    })
+        .then(response => {
+            console.log('response: ',response)
+            return response.json()
+        })
+        .then(jsonResponse => {
+            if (jsonResponse.error) {
+                console.log('jsonResponse.error: ', jsonResponse.error)
+                toast.error(jsonResponse.error)
+                return
+            }
+
+            console.log('Registration Successful!')
+            toast.success("Registration Successful!")
+
+            navigate('/login')
+        })
+        .catch(err => {
+            console.log('err: ',err)
+            toast.error(err.message)
+        })
   };
 
   return (
@@ -51,18 +130,23 @@ export default function Register() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -73,6 +157,8 @@ export default function Register() {
                   id="name"
                   label="Name"
                   autoFocus
+                  value={formData.name}
+                  onChange={handleChange}
                 />
               </Grid>
 
@@ -84,6 +170,8 @@ export default function Register() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={formData.email}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -95,6 +183,8 @@ export default function Register() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={formData.password}
+                  onChange={handleChange}
                 />
               </Grid>
 
@@ -106,6 +196,8 @@ export default function Register() {
                   label="Job Title"
                   name="job"
                   autoComplete="job"
+                  value={formData.job}
+                  onChange={handleChange}
                 />
               </Grid>
 
@@ -117,45 +209,55 @@ export default function Register() {
                   label="Position"
                   name="position"
                   autoComplete="position"
+                  value={formData.position}
+                  onChange={handleChange}
                 />
               </Grid>
 
               <Grid item xs={12}>
                 <Typography id="input-slider" gutterBottom>
-                    Experience*
+                  Experience*
                 </Typography>
-                <Slider defaultValue={0} aria-label="Default" valueLabelDisplay="auto" max={50} 
-                id="experience"
-                name="experience"
-                label="experience"
+                <Slider
+                  defaultValue={0}
+                  aria-label="Default"
+                  valueLabelDisplay="auto"
+                  max={50}
+                  id="experience"
+                  name="experience"
+                  label="experience"
+                  value={formData.experience}
+                  onChange={handleChange}
                 />
-
               </Grid>
 
               <Grid item xs={12}>
                 <Stack spacing={3} sx={{ width: 500 }}>
-                    <Autocomplete
-                        multiple
-                        id="skills"
-                        options={skills}
-                        getOptionLabel={(option) => option.skill}
-                        defaultValue={[skills[0]]}
-                        renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            variant="standard"
-                            label="Add Your Tech Stack"
-                            placeholder="Skills"
-                        />
-                        )}
-                    />
+                  <Autocomplete
+                    multiple
+                    id="skills"
+                    // value={formData.skills}
+                    // onChange={handleChange}
+                    options={setskills}
+                    getOptionLabel={(option) => option.skill}
+                    defaultValue={[setskills[0]]}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="standard"
+                        label="Add Your Tech Stack"
+                        placeholder="Skills"
+                      />
+                    )}
+                  />
                 </Stack>
               </Grid>
 
-
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive alerts on new jobs via email."
                 />
               </Grid>
@@ -181,9 +283,14 @@ export default function Register() {
       </Container>
     </ThemeProvider>
   );
-
-
-
 }
 
-const skills = [{skill:'HTML'}, {skill:'CSS'}, {skill:'JavaScript'}, {skill:'React'}, {skill:'Node'}, {skill:'Mongo'}, {skill:'Express'}]
+const setskills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "React" },
+  { skill: "Node" },
+  { skill: "Mongo" },
+  { skill: "Express" },
+];
